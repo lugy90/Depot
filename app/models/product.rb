403c4d3d3b82ae3c1d08a840530365486ba19c6f
lugy90 <naitianzong@outlook.com>
2015-07-21 19:22:25 +0800

    complete check out function

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,8 @@ class Product < ActiveRecord::Base
 	end
 
 	has_many :line_items
+  has_many :orders, through: :line_items
+
 	# ensure that there are no line items referencing this product
 	before_destroy :ensure_not_referenced_by_any_line_item
 	def ensure_not_referenced_by_any_line_item
